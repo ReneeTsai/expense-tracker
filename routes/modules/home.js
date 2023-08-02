05020687c9d94fa9d5c5f81cc3630b91fb3bfbaa
Rachel <juoruel@gmail.com>
2023-08-02 23:43:18 +0800

    router home search category and sum

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,14 +2,41 @@
 const express = require("express");
 const router = express.Router();
 const Record = require("../../models/record");
+const Category = require("../../models/category")
 const { sum } = require("../../function/sum");
 // 準備引入路由模組
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+ try {
   Record.find()
-    .lean()
-    .sort({ _id: "asc" })
-    .then((records) => res.render("index", { records }))
-    .catch((error) => console.error(error));
-});
+  .lean()
+  .sort({ _id: "asc" })
+  .then((records) => {
+    // 將日期格式進行轉換
+    records.forEach((record) => {
+      record.date = new Date(record.date).toLocaleDateString();
+    });
+    res.render("index", { records ,sum: sum(records)})
+  })
+ }catch (err) {
+  console.log(err)
+}
+})
+// 選取特定類別的紀錄
+router.get("/search", async (req, res) => {
+  const category = req.query.category
+  try {
+    const categoryWithId = await Category.findOne({ name: category }) .lean()
+    const records = await Record.find({ categoryId: categoryWithId._id}) .lean()
+    const sortedRecords = await Promise.all(records.map(async record => {
+    return {
+      ...record,
+      date: new Date(record.date).toLocaleDateString(),
+      image: categoryWithId.icon
+    }}))
+    res.render("index", { records: sortedRecords, sum: sum(records) })
+  } catch (err) {
+    console.log(err)
+  }
+})
 
 module.exports = router;
